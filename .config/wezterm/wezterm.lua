7e7e448bd9962eab247c5548626ee61b8cf5dbdb
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-07-18 12:47:51 +0800

    Update Tmux Config with Navigation Improvements

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,10 +8,10 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 -- config.color_scheme = 'ayu'
--- config.color_scheme = 'Brogrammer'
+config.color_scheme = 'Brogrammer'
 -- config.color_scheme = 'GitHub Dark'
 -- config.color_scheme = 'Glacier'
-config.color_scheme = 'Gotham (terminal.sexy)'
+-- config.color_scheme = 'Gotham (terminal.sexy)'
 -- config.color_scheme = 'Vacuous 2 (terminal.sexy)'
 
 config.initial_cols = 100
@@ -20,6 +20,9 @@ config.initial_rows = 30
 config.font = wezterm.font_with_fallback({'MesloLGS Nerd Font', 'JetBrains Mono'}, { weight = 'Regular' })
 config.font_size = 14.0
 
+-- Hide Tabs
+config.enable_tab_bar = false
+
 -- Support option+left/right on mac to jump words
 -- https://github.com/wez/wezterm/issues/253#issuecomment-672007120
 config.keys = {
@@ -27,6 +30,12 @@ config.keys = {
   {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
   -- Make Option-Right equivalent to Alt-f; forward-word
   {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+
+  -- Switch Tabas
+  -- Same as in Chrome
+  -- macOS
+  {key="LeftArrow", mods="CMD|OPT", action=wezterm.action{ActivateTabRelative=-1}},
+  {key="RightArrow", mods="CMD|OPT", action=wezterm.action{ActivateTabRelative=1}},
 }
 
 -- and finally, return the configuration to wezterm
