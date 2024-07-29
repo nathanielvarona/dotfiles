e200daae54a47c88333e81246305d7b98f69f852
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-07-29 22:42:19 +0800

    Options for Powerlevel10k Custom or Builtin Configs

---
+++ .p10k-custom.zsh
@@ -0,0 +1,39 @@
+# Prompt Mode
+POWERLEVEL9K_MODE=nerdfont-v3
+POWERLEVEL9K_DISABLE_RPROMPT=true
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX='❱ '
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=''
+POWERLEVEL9K_STATUS_OK_IN_NON_VERBOSE=true
+POWERLEVEL9K_STATUS_VERBOSE=false
+
+# Prompt Figures
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+  os_icon
+  dir
+  vcs
+  direnv
+  asdf
+  fnm
+  rbenv
+  pyenv
+  virtualenv
+  anaconda
+  go_version
+  rust_version
+  terraform
+  aws
+  gcloud
+  google_app_cred
+  azure
+  kubecontext
+  docker_machine
+  background_jobs
+  command_execution_time
+  status
+)
+
+# Truncating Long Directories
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
+# POWERLEVEL9K_SHORTEN_DELIMITER=""
+# POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
--- .p10k.zsh
+++ .p10k.zsh
@@ -1,39 +1,27 @@
-# Prompt Mode
-POWERLEVEL9K_MODE=nerdfont-v3
-POWERLEVEL9K_DISABLE_RPROMPT=true
-POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX='❱ '
-POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=''
-POWERLEVEL9K_STATUS_OK_IN_NON_VERBOSE=true
-POWERLEVEL9K_STATUS_VERBOSE=false
+# Get the desired config index (default to 7: Robby Russell)
+p10k_config_index=1
 
-# Prompt Figures
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-  os_icon
-  dir
-  vcs
-  direnv
-  asdf
-  fnm
-  rbenv
-  pyenv
-  virtualenv
-  anaconda
-  go_version
-  rust_version
-  terraform
-  aws
-  gcloud
-  google_app_cred
-  azure
-  kubecontext
-  docker_machine
-  background_jobs
-  command_execution_time
-  status
+# Function to retrieve the path to the Powerlevel10k config directory
+_get_p10k_config_path() {
+  local config_path="$(find "$HOME/.local/share/zinit/plugins" -name 'romkatv---powerlevel10k')/config"
+  [[ -d "$config_path" ]] || { echo "Error: Powerlevel10k config directory not found"; exit 1; }
+  echo "$config_path"
+}
+
+# Array of potential Powerlevel10k config files
+# Indices correspond to config numbers
+p10k_configs=(
+  "$HOME/.p10k-custom.zsh"  # 1: Custom config
+  "$(_get_p10k_config_path)/p10k-classic.zsh"  # 2: Classic
+  "$(_get_p10k_config_path)/p10k-lean-8colors.zsh"  # 3: Lean 8 colors
+  "$(_get_p10k_config_path)/p10k-lean.zsh"  # 4: Lean
+  "$(_get_p10k_config_path)/p10k-pure.zsh"  # 5: Pure
+  "$(_get_p10k_config_path)/p10k-rainbow.zsh"  # 6: Rainbow
+  "$(_get_p10k_config_path)/p10k-robbyrussell.zsh"  # 7: Robby Russell
 )
 
-# Truncating Long Directories
-POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
-# POWERLEVEL9K_SHORTEN_DELIMITER=""
-# POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
+# Get the config path based on the index
+p10k_config="${p10k_configs[$p10k_config_index]}"
+
+# Source the selected config file
+source "$p10k_config"
