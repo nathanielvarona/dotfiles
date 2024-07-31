411c2fc6259f852a78f3da38cf8a3c1bff8c94d0
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-07-31 18:23:32 +0800

    Update VSCode and NeoVim Settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Load custom autocmds
+require("config.autocmds")
+
+-- Load custom keymaps
+require("config.keymaps")
+
+-- Load custom options
+require("config.options")
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Check if running inside VSCode
+-- if vim.g.vscode then
+--   -- settings only for vscode
+-- end
