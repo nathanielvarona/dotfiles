202a29f31be3d44f6b036e8085809b741c254616
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-07-31 19:44:26 +0800

    Restore LazyVim Config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- Load custom autocmds
-require("config.autocmds")
-
--- Load custom keymaps
-require("config.keymaps")
-
--- Load custom options
-require("config.options")
