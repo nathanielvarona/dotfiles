c16ed23a76acf9c2b4f109d50572075c091dbfa2
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-08-06 23:40:38 +0800

    Format Shell Scripts for Powerlevel10k

--- .p10k.zsh
+++ .p10k.zsh
@@ -1,23 +1,26 @@
-# Get the desired config index (default to 7: Robby Russell)
+# Get the desired config index (default to 1: Custom Minimal)
 p10k_config_index=1
 
 # Function to retrieve the path to the Powerlevel10k config directory
 _get_p10k_config_path() {
   local config_path="$(find "$HOME/.local/share/zinit/plugins" -name 'romkatv*' | grep --color=never 'powerlevel10k')/config"
-  [[ -d "$config_path" ]] || { echo "Error: Powerlevel10k config directory not found"; exit 1; }
+  [[ -d "$config_path" ]] || {
+    echo "Error: Powerlevel10k config directory not found"
+    exit 1
+  }
   echo "$config_path"
 }
 
 # Array of potential Powerlevel10k config files
 # Indices correspond to config numbers
 p10k_configs=(
-  "$HOME/.p10k-custom.zsh"  # 1: Custom config
-  "$(_get_p10k_config_path)/p10k-classic.zsh"  # 2: Classic
-  "$(_get_p10k_config_path)/p10k-lean-8colors.zsh"  # 3: Lean 8 colors
-  "$(_get_p10k_config_path)/p10k-lean.zsh"  # 4: Lean
-  "$(_get_p10k_config_path)/p10k-pure.zsh"  # 5: Pure
-  "$(_get_p10k_config_path)/p10k-rainbow.zsh"  # 6: Rainbow
-  "$(_get_p10k_config_path)/p10k-robbyrussell.zsh"  # 7: Robby Russell
+  "$HOME/.p10k-custom.zsh"                         # 1: Custom Minimal
+  "$(_get_p10k_config_path)/p10k-classic.zsh"      # 2: Classic
+  "$(_get_p10k_config_path)/p10k-lean-8colors.zsh" # 3: Lean 8 colors
+  "$(_get_p10k_config_path)/p10k-lean.zsh"         # 4: Lean
+  "$(_get_p10k_config_path)/p10k-pure.zsh"         # 5: Pure
+  "$(_get_p10k_config_path)/p10k-rainbow.zsh"      # 6: Rainbow
+  "$(_get_p10k_config_path)/p10k-robbyrussell.zsh" # 7: Robby Russell
 )
 
 # Get the config path based on the index
