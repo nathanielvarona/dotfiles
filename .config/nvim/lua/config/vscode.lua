1f554296f584f548565757df3453ac2225db0628
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-08-08 22:00:45 +0800

    `vscode-neovim` Extended User-taste Configurations

---
+++ .config/nvim/lua/config/vscode.lua
@@ -0,0 +1,58 @@
+--[[
+  This file contains configuration settings specifically for the `vscode-neovim` extension.
+
+  For more information about the `vscode-neovim` extension and its API usage,
+  refer to the official documentation and repositories:
+
+  GitHub Repository:
+    - https://github.com/vscode-neovim/vscode-neovim
+
+  Visual Studio Code Marketplace:
+    - https://marketplace.visualstudio.com/items?itemName=asvetliakov.vscode-neovim
+]]
+
+if vim.g.vscode then
+  local vscode = require("vscode")
+  local mappings = {
+    up = "k",
+    down = "j",
+    wrappedLineStart = "0",
+    wrappedLineFirstNonWhitespaceCharacter = "^",
+    wrappedLineEnd = "$",
+  }
+
+  local function moveCursor(to, select)
+    return function()
+      local mode = vim.api.nvim_get_mode()
+      if mode.mode == "V" or mode.mode == "" then
+        return mappings[to]
+      end
+
+      vscode.action("cursorMove", {
+        args = {
+          {
+            to = to,
+            by = "wrappedLine",
+            value = vim.v.count1,
+            select = select,
+          },
+        },
+      })
+      return "<Ignore>"
+    end
+  end
+
+  vim.keymap.set("n", "k", moveCursor("up"), { expr = true })
+  vim.keymap.set("n", "j", moveCursor("down"), { expr = true })
+  vim.keymap.set("n", "0", moveCursor("wrappedLineStart"), { expr = true })
+  vim.keymap.set("n", "^", moveCursor("wrappedLineFirstNonWhitespaceCharacter"), { expr = true })
+  vim.keymap.set("n", "$", moveCursor("wrappedLineEnd"), { expr = true })
+
+  vim.keymap.set("v", "k", moveCursor("up", true), { expr = true })
+  vim.keymap.set("v", "j", moveCursor("down", true), { expr = true })
+  vim.keymap.set("v", "0", moveCursor("wrappedLineStart", true), { expr = true })
+  vim.keymap.set("v", "^", moveCursor("wrappedLineFirstNonWhitespaceCharacter", true), { expr = true })
+  vim.keymap.set("v", "$", moveCursor("wrappedLineEnd", true), { expr = true })
+
+  vim.notify("vscode-specific configuration loaded...")
+end
