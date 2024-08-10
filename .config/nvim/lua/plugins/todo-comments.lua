73e9a6bebc8db4426e0969cd3a9bae6e737afb03
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-08-10 10:26:33 +0800

    Update NeoVim `todo-comments.nvim` and VSCode `vscode-todo-highlight`to works the same

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -28,9 +28,9 @@ return {
         bug = { "#f9e2af" }, -- Yellow (BUG)
       },
       highlight = {
-        before = "fg", -- "fg" or "bg" or empty
+        before = "empty", -- "fg" or "bg" or empty
         keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-        after = "fg", -- "fg" or "bg" or empty
+        after = "empty", -- "fg" or "bg" or empty
         pattern = [[.*<(KEYWORDS)\s*:]], -- pattern used for highlight (vim regex)
         comments_only = true, -- uses treesitter to match keywords in comments only
         max_line_len = 400, -- ignore lines longer than this
