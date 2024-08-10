162eaa4b7835d93fccefea327bfa05968be35bf8
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-08-11 02:21:50 +0800

    Update Todo Comments Tags on Neovim

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -36,7 +36,7 @@ return {
         keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
         after = "empty", -- "fg" or "bg" or empty
         pattern = [[.*<(KEYWORDS)\s*:]], -- pattern used for highlight (vim regex)
-        comments_only = true, -- uses treesitter to match keywords in comments only
+        comments_only = false, -- uses treesitter to match keywords in comments only
         max_line_len = 400, -- ignore lines longer than this
         exclude = {}, -- list of file types to exclude highlighting
       },
