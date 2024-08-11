0c1246660852e48ce18cc4fd03f8ee5c2a54da91
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-08-12 01:39:17 +0800

    Neovim Colorscheme as LazyVim Override Method

--- .config/lazyvim/lua/plugins/colorscheme.lua
+++ .config/lazyvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,12 @@
 return {
-  "projekt0n/github-nvim-theme",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require("github-theme").setup({
-      -- ...
-    })
-
-    -- vim.cmd("colorscheme github_dark_default")
-    vim.cmd("colorscheme github_dark_high_contrast")
-    -- vim.cmd('colorscheme github_light_default')
-    -- vim.cmd('colorscheme github_light_high_contrast')
-  end,
+  { "projekt0n/github-nvim-theme" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "github_dark_default",
+      colorscheme = "github_dark_high_contrast",
+      -- colorscheme = "github_light_default",
+      -- colorscheme = "github_light_high_contrast",
+    },
+  },
 }
