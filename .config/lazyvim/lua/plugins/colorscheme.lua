d0b7446a1cc5f6642583148631461ea291ee2053
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2025-09-24 22:26:33 +0800

    Overide LazyVim `colorscheme` Options using Custom Extended ColorScheme Plugins

--- .config/lazyvim/lua/plugins/colorscheme.lua
+++ .config/lazyvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,22 @@
 return {
+  -- Overide LazyVim `colorscheme` Settings
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_high_contrast",
+      -- colorscheme = "github_dark_tritanopia",
+    },
+  },
+
+  -- Extend Custom Colorscheme Options
   {
     "projekt0n/github-nvim-theme",
-    tag = "v1.1.2",
-    -- enabled = false,
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = false,
+    priority = 1000,
     config = function()
       require("github-theme").setup({
-        -- ...
+        -- your custom options here
       })
-
-      vim.cmd("colorscheme github_dark_high_contrast")
     end,
   },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     -- colorscheme = "github_dark_default",
-  --     colorscheme = "github_dark_high_contrast",
-  --     -- colorscheme = "github_light_default",
-  --     -- colorscheme = "github_light_high_contrast",
-  --   },
-  -- },
 }
