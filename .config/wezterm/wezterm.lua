880c80d5647e6fbf9676eb7d62ccf57ffadf3fa5
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2025-09-25 00:49:11 +0800

    Change Wezterm Terminal Theme to Brogrammer

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "GitHub Dark"
+config.color_scheme = "Brogrammer"
 
 -- Intended for Recommended Asciinema Demo
 config.initial_cols = 100
