68b8da3e08f12eaabf83b5f54f578f2e4c424b17
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2025-09-29 12:28:46 +0800

    Hardtime Default Options

--- .config/lazyvim/lua/plugins/hardtime.lua
+++ .config/lazyvim/lua/plugins/hardtime.lua
@@ -3,8 +3,6 @@ return {
     "m4xshen/hardtime.nvim",
     lazy = false,
     dependencies = { "MunifTanjim/nui.nvim" },
-    opts = function()
-      require("hardtime").setup({})
-    end,
+    opts = {},
   },
 }
