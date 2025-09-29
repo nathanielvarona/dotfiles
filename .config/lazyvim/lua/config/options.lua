15d14348d00d414943bdd4c2c25624651f01a33c
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2025-09-29 19:39:40 +0800

    Enable Lazy Extras Use the Picker

--- .config/lazyvim/lua/config/options.lua
+++ .config/lazyvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.lazyvim_picker = "fzf"
+-- vim.g.lazyvim_picker = "fzf"
 -- vim.g.lazyvim_picker = "telescope"
 
 -- The order of your `lazy.nvim` imports is incorrect:
