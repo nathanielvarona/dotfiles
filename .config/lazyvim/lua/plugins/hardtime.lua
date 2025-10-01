e317cdfbc97c7358e5df9c5bb33f08df8b608ad1
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2025-10-02 04:26:21 +0800

    Fix hardtime.nvim Plugin works accross all projects

--- .config/lazyvim/lua/plugins/hardtime.lua
+++ .config/lazyvim/lua/plugins/hardtime.lua
@@ -1,8 +1,27 @@
 return {
   {
     "m4xshen/hardtime.nvim",
-    lazy = false,
+    event = "VeryLazy",
     dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
+    config = function()
+      require("hardtime").setup({
+
+        hints = {
+          ["k%^"] = {
+            message = function()
+              return "Use - instead of k^" -- return the hint message you want to display
+            end,
+            length = 2, -- the length of actual key strokes that matches this pattern
+          },
+          ["d[tTfF].i"] = { -- this matches d + {t/T/f/F} + {any character} + i
+            message = function(keys) -- keys is a string of key strokes that matches the pattern
+              return "Use " .. "c" .. keys:sub(2, 3) .. " instead of " .. keys
+              -- example: Use ct( instead of dt(i
+            end,
+            length = 4,
+          },
+        },
+      })
+    end,
   },
 }
