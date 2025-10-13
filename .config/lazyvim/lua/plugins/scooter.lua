0f57e1af9375394bd2e2262f871607a467eae2c6
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2025-10-13 21:31:03 +0800

    Remove ToggleTerm in favor of Snacks for Scooter Search and Replace

--- .config/lazyvim/lua/plugins/scooter.lua
+++ .config/lazyvim/lua/plugins/scooter.lua
@@ -2,70 +2,59 @@
 -- if true then return {} end
 
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-      local Terminal = require("toggleterm.terminal").Terminal
-      local scooter_term = nil
+  "folke/snacks.nvim",
+  config = function()
+    local scooter_term = nil
 
-      -- Open existing scooter terminal if one is available, otherwise create a new one
-      local function open_scooter()
-        if not scooter_term then
-          scooter_term = Terminal:new({
-            cmd = "scooter",
-            direction = "float",
-            close_on_exit = true,
-            on_exit = function()
-              scooter_term = nil
-            end,
-          })
-        end
-        scooter_term:open()
+    -- Called by scooter to open the selected file at the correct line from the scooter search list
+    _G.EditLineFromScooter = function(file_path, line)
+      if scooter_term and scooter_term:buf_valid() then
+        scooter_term:hide()
       end
 
-      -- Called by scooter to open the selected file at the correct line from the scooter search list
-      _G.EditLineFromScooter = function(file_path, line)
-        if scooter_term and scooter_term:is_open() then
-          scooter_term:close()
-        end
+      local current_path = vim.fn.expand("%:p")
+      local target_path = vim.fn.fnamemodify(file_path, ":p")
 
-        local current_path = vim.fn.expand("%:p")
-        local target_path = vim.fn.fnamemodify(file_path, ":p")
-
-        if current_path ~= target_path then
-          vim.cmd.edit(vim.fn.fnameescape(file_path))
-        end
-
-        vim.api.nvim_win_set_cursor(0, { line, 0 })
+      if current_path ~= target_path then
+        vim.cmd.edit(vim.fn.fnameescape(file_path))
       end
 
-      -- Opens scooter with the search text populated by the `search_text` arg
-      _G.OpenScooterSearchText = function(search_text)
-        if scooter_term and scooter_term:is_open() then
-          scooter_term:close()
-        end
+      vim.api.nvim_win_set_cursor(0, { line, 0 })
+    end
 
-        local escaped_text = vim.fn.shellescape(search_text:gsub("\r?\n", " "))
-        scooter_term = Terminal:new({
-          cmd = "scooter --search-text " .. escaped_text,
-          direction = "float",
-          close_on_exit = true,
-          on_exit = function()
-            scooter_term = nil
-          end,
+    local function is_terminal_running(term)
+      if not term or not term:buf_valid() then
+        return false
+      end
+      local channel = vim.fn.getbufvar(term.buf, "terminal_job_id")
+      return channel and vim.fn.jobwait({ channel }, 0)[1] == -1
+    end
+
+    local function open_scooter()
+      if is_terminal_running(scooter_term) then
+        scooter_term:toggle()
+      else
+        scooter_term = require("snacks").terminal.open("scooter", {
+          win = { position = "float" },
         })
-        scooter_term:open()
+      end
+    end
+
+    local function open_scooter_with_text(search_text)
+      if scooter_term and scooter_term:buf_valid() then
+        scooter_term:close()
       end
 
-      vim.keymap.set("n", "<leader>sf", open_scooter, { desc = "Open scooter" })
+      local escaped_text = vim.fn.shellescape(search_text:gsub("\r?\n", " "))
+      scooter_term = require("snacks").terminal.open("scooter --fixed-strings --search-text " .. escaped_text, {
+        win = { position = "float" },
+      })
+    end
 
-      vim.keymap.set(
-        "v",
-        "<leader>sf",
-        '"ay<ESC><cmd>lua OpenScooterSearchText(vim.fn.getreg("a"))<CR>',
-        { desc = "Search selected text in scooter" }
-      )
-    end,
-  },
+    vim.keymap.set("n", "<leader>sf", open_scooter, { desc = "Open Scooter" })
+    vim.keymap.set("v", "<leader>sf", function()
+      local search_text = vim.fn.getregion()
+      open_scooter_with_text(search_text)
+    end, { desc = "Open Scooter with selected text" })
+  end,
 }
