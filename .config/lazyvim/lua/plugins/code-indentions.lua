6bb74ce811d0acc5fc5a79794b4137ef4b928f1b
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2025-10-24 11:02:10 +0800

    Remove NeoScroll and SmearCursor in favor of LazyVim Extras

--- .config/lazyvim/lua/plugins/code-indentions.lua
+++ .config/lazyvim/lua/plugins/code-indentions.lua
@@ -1,3 +1,6 @@
+-- stylua: ignore
+-- if true then return {} end
+
 local highlight = {
   "RainbowRed",
   "RainbowYellow",
--- .config/lazyvim/lua/plugins/hardtime.lua
+++ .config/lazyvim/lua/plugins/hardtime.lua
@@ -1,3 +1,6 @@
+-- stylua: ignore
+if true then return {} end
+
 return {
   {
     "m4xshen/hardtime.nvim",
