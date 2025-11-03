851606802db38db3409e264b7ebb74ef2e728e8a
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2025-11-04 02:05:50 +0800

    Use `pyright` for Python LSP

--- .config/lazyvim/lua/plugins/treesitter.lua
+++ .config/lazyvim/lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
+-- stylua: ignore
+if true then return {} end
+
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     return vim.tbl_deep_extend("force", opts, {
       ensure_installed = {
-        "latex",
         "norg",
-        "svelte",
-        "typst",
-        "vue",
       },
     })
   end,
