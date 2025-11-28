aea1c2755a6cbee4f0641e21f9d15912aa4e8983
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2025-11-29 01:14:53 +0800

    Nvim and Tmux Seamless Panes Navigation and Resizing

---
+++ .config/lazyvim/lua/plugins/tmux.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "aserowy/tmux.nvim",
+    config = function()
+      return require("tmux").setup({
+        copy_sync = {
+          enable = false,
+        },
+        navigation = {
+          enable_default_keybindings = false,
+        },
+      })
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+}
