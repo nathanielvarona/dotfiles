551bf1befb1e360ada8f6f10cac29aa8783c5954
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2026-01-28 23:09:36 +0800

    Fix Extending Default Treesitter Parsers

--- .config/lazyvim/lua/plugins/treesitter.lua
+++ .config/lazyvim/lua/plugins/treesitter.lua
@@ -1,13 +1,8 @@
--- stylua: ignore
--- if true then return {} end
-
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    return vim.tbl_deep_extend("force", opts, {
-      ensure_installed = {
-        "zsh",
-      },
-    })
-  end,
+  opts = {
+    ensure_installed = {
+      "zsh",
+    },
+  },
 }
