498230fe5fd98af6611126f075146c2823a1486d
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2026-02-10 21:19:32 +0800

    Explicitly Set the Leader Key

--- .config/lazyvim/lua/config/options.lua
+++ .config/lazyvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Leader Key
+vim.g.mapleader = " "
+
 -- vim.g.lazyvim_picker = "fzf"
 -- vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_picker = "snacks"
