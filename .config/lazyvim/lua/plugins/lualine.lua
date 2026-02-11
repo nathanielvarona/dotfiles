27fa6a853ba5e8b67283a67519e4b398be3940e8
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2026-02-11 11:15:20 +0800

    Nvim Lualine Modes in Shorthand

--- .config/lazyvim/lua/plugins/lualine.lua
+++ .config/lazyvim/lua/plugins/lualine.lua
@@ -1,7 +1,41 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
-    opts.options.section_separators = { left = "", right = "" }
-    opts.options.component_separators = { left = "│", right = "│" }
+    opts.options.section_separators = {
+      left = "",
+      right = "",
+    }
+    opts.options.component_separators = {
+      left = "│",
+      right = "│",
+    }
+
+    local mode_map = {
+      NORMAL = "N",
+      ["O-PENDING"] = "O",
+      INSERT = "I",
+      VISUAL = "V",
+      ["V-BLOCK"] = "VB",
+      ["V-LINE"] = "VL",
+      ["V-REPLACE"] = "VR",
+      REPLACE = "R",
+      COMMAND = "C",
+      ["COMMAND-LINE"] = "CL",
+      EX = "X",
+      MORE = "M",
+      CONFIRM = "Y",
+      SELECT = "S",
+      TERMINAL = "T",
+    }
+
+    opts.sections = opts.sections or {}
+    opts.sections.lualine_a = {
+      {
+        "mode",
+        fmt = function(res)
+          return mode_map[res] or res
+        end,
+      },
+    }
   end,
 }
