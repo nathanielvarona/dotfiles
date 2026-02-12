2d5c57b73c3dbc3f52c175d27cb8a5635f9ea534
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2026-02-12 13:47:45 +0800

    Simple Colorscheme Options

--- .config/lazyvim/lua/plugins/colorscheme.lua
+++ .config/lazyvim/lua/plugins/colorscheme.lua
@@ -1,38 +1,14 @@
 return {
-  -- Extend Custom Colorscheme Options
-  {
-    "projekt0n/github-nvim-theme",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("github-theme").setup({
-        -- your custom options here
-      })
-      -- vim.cmd("colorscheme github_dark_high_contrast")
-      -- vim.cmd("colorscheme github_dark_tritanopia")
-    end,
-  },
+  -- Other Custom Colorscheme
+  { "projekt0n/github-nvim-theme" },
 
-  -- { "rebelot/kanagawa.nvim" },
-
-  -- { "ellisonleao/gruvbox.nvim" },
-
-  -- { "tiagovla/tokyodark.nvim" },
-
-  -- Overide LazyVim `colorscheme` Settings
+  -- Apply Colorscheme via LazyVim Options
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "tokyonight-night",
       -- colorscheme = "catppuccin-mocha",
-
-      -- colorscheme = "github_dark_default",
       colorscheme = "github_dark_high_contrast",
-      -- colorscheme = "github_dark_tritanopia",
-      -- colorscheme = "kanagawa-dragon",
-      -- colorscheme = "kanagawa-wave",
-      -- colorscheme = "gruvbox",
-      -- colorscheme = "tokyodark",
     },
   },
 }
