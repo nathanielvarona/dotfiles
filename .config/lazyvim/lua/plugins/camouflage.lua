176a211255e743037eca923e555a5f20be25e9d9
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2026-02-13 03:48:19 +0800

    Prevent duplicate patterns on multiple reloads for `camouflage.nvim` plugin config

--- .config/lazyvim/lua/plugins/camouflage.lua
+++ .config/lazyvim/lua/plugins/camouflage.lua
@@ -7,6 +7,15 @@ return {
 
     local defaults = require("camouflage.config").defaults
 
+    local function add_unique_pattern(list, pattern)
+      for _, p in ipairs(list) do
+        if vim.deep_equal(p, pattern) then
+          return
+        end
+      end
+      table.insert(list, pattern)
+    end
+
     -- Start with a copy of defaults
     local config = vim.deepcopy(defaults)
 
@@ -24,19 +33,25 @@ return {
 
     -- Extend patterns
     config.patterns = config.patterns or {}
-    vim.list_extend(config.patterns, {
-      {
-        file_pattern = { "*.secrets", ".secrets*" },
-        parser = "env",
+
+    add_unique_pattern(
+      config.patterns,
+      { file_pattern = {
+        "*.secrets",
+        ".secrets*",
+      }, parser = "env" }
+    )
+
+    add_unique_pattern(config.patterns, {
+      file_pattern = {
+        "environment.plist",
       },
-      {
-        file_pattern = { "environment.plist" },
-        parser = "xml",
-      },
+      parser = "xml",
     })
 
     return config
   end,
+
   keys = {
     { "<leader>ct", "<cmd>CamouflageToggle<cr>", desc = "Toggle Camouflage" },
     { "<leader>cr", "<cmd>CamouflageReveal<cr>", desc = "Reveal Line" },
