1d65feb3f2f4179ebeaf682f9b1fa02fbb87167a
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2026-05-11 20:17:50 +0800

    Format LazyGit External Diff Command Script

--- dot_config/lazygit/win-git-delta-compat.bat
+++ dot_config/lazygit/win-git-delta-compat.bat
@@ -7,6 +7,6 @@ set "old=%old:\=/%"
 set "new=%new:\=/%"
 
 git --no-pager diff --no-index --no-ext-diff "%old%" "%new%"^
- | sed -e "s|%old%|%target%|g" -e "s|%new%|%target%|g"^
- | delta --paging=never --line-numbers --width=%LAZYGIT_COLUMNS%
+  | sed -e "s|%old%|%target%|g" -e "s|%new%|%target%|g"^
+  | delta --paging=never --line-numbers --width=%LAZYGIT_COLUMNS%
 
