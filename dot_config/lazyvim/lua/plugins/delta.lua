0c3f97984251f5b45742540d464f5a66cfbba33a
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2026-06-15 11:06:13 +0800

    Nvim Delta is merged to DeltaView Plugin

--- dot_config/lazyvim/lua/plugins/delta.lua
+++ dot_config/lazyvim/lua/plugins/delta.lua
@@ -1,16 +1,5 @@
 return {
   {
-    "kokusenz/delta.lua",
-    config = function()
-      require("delta").setup({
-        highlighting = {
-          max_similarity_threshold = 0.4,
-        },
-      })
-    end,
-  },
-
-  {
     "kokusenz/deltaview.nvim",
     config = function()
       require("deltaview").setup({})
