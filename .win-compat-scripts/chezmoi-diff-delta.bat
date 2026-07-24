b78223c583f73ab6fad722ad9a159490fdf370a5
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2026-07-24 23:59:46 +0800

    Fix Chezmoi Diff for New Files especially Run Scripts

--- .win-compat-scripts/chezmoi-diff-delta.bat
+++ .win-compat-scripts/chezmoi-diff-delta.bat
@@ -1,9 +1,11 @@
 @echo off
+setlocal
 
 set "old=%~1"
 set "new=%~2"
-set "old=%old:\=/%"
-set "new=%new:\=/%"
+
+if not exist "%old%" set "old=/dev/null"
+if not exist "%new%" set "new=/dev/null"
 
 git --no-pager diff --no-index --no-ext-diff "%old%" "%new%" ^
   | delta --paging=never --line-numbers
