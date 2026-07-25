263fa65923a279bde5a442b81c4b6935d3211edc
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2026-07-26 01:25:31 +0800

    Added PSMux Plugin Manager

--- .psmux.ps1
+++ .psmux.ps1
@@ -13,24 +13,24 @@ psmux new-session -d -s 'dotfiles'
 psmux split-window -v -p 20 -t 'dotfiles:0.0'
 
 # Focus the top pane (0) and open the editor
-psmux send-keys -t 'dotfiles:0.0' 'nvim .' Enter
+psmux send-keys -t 'dotfiles:1.0' 'nvim .' Enter
 # Run list command in bottom 20% pane (1)
-psmux send-keys -t 'dotfiles:0.1' 'ls -a' Enter
+psmux send-keys -t 'dotfiles:1.1' 'ls -a' Enter
 # Force final cursor focus onto the editor pane
-psmux select-pane -t 'dotfiles:0.0'
+psmux select-pane -t 'dotfiles:1.0'
 
 # --- WINDOW 1: vcs ---
 # Create second window
 psmux new-window -t 'dotfiles'
 # Split window vertically and assign the NEW bottom pane 20% size
-psmux split-window -v -p 20 -t 'dotfiles:1.0'
+psmux split-window -v -p 20 -t 'dotfiles:2.0'
 
 # Run git client in top 80% pane (0)
-psmux send-keys -t 'dotfiles:1.0' 'lazygit' Enter
+psmux send-keys -t 'dotfiles:2.0' 'lazygit' Enter
 # Run version check in bottom 20% pane (1)
-psmux send-keys -t 'dotfiles:1.1' 'git --version' Enter
+psmux send-keys -t 'dotfiles:2.1' 'git --version' Enter
 
 # --- FINAL ATTACH ---
 # Switch active view back to the editor window and attach
-psmux select-window -t 'dotfiles:0'
+psmux select-window -t 'dotfiles:1'
 psmux attach-session -t 'dotfiles'
